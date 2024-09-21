e0c02cdfe4eb25dc45cda9413daacd94e3722b64
Zhiwen huang <zhiwen555@gmail.com>
2024-09-20 18:19:26 -0700

    Made progress

--- client/src/App.js
+++ client/src/App.js
@@ -78,8 +78,8 @@ function App() {
               navBarItems[i].style.marginBottom = "0px";
             } else {
               navBarItems[i].style.visibility = "visible";
-              navBarItems[i].style.height = "auto";
-              navBarItems[i].style.width = "auto";
+              navBarItems[i].style.height = "200px";
+              navBarItems[i].style.width = "200px";
               navBarItems[i].style.marginBottom = "3px";
             }
           }
